f527c5aecf480919f6d3b4d9488f8a4dfc8d26a3
ssp <laokiea@163.com>
2017-10-12 12:55:24 +0800

    das

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php 
 return [
-	"keys" => ['accessKeyId' => 'LTAItQbfYTW5pAI9' ,'accessKeySecret' => 'iU2Ye1dV1BGQkTvt6h1m34j553pmnG'],
+	"keys" => ['accessKeyId' => '' ,'accessKeySecret' => ''],
 	"sign" => "归途",
 	"tmp_id" => "SMS_102320016",
 	"phone" => "18355613657",
 	// database
 	'dsn' => 'mysql:dbname=qdm177116231_db;host=qdm177116231.my3w.com',
-	'pass' => 'Ssp6315417',
+	'pass' => '',
 	'user' => 'qdm177116231',
 	//ip
 	'ip_api' => 'http://ip.taobao.com/service/getIpInfo.php?ip=',
